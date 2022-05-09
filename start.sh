6d0f2d01c75fa827c4ef1641e265e53b602d2f0b
Robert Hedges <hedges@ieee.org>
2022-05-08 21:32:16 -0500

    use new yoctodocker image in startup.sh

--- start.sh
+++ start.sh
@@ -80,19 +80,19 @@ if [ "${run_additional_instance}" = true ]; then
         -it \
         --user yocto \
         -w /opt/yocto/workspace \
-        hubshuffle-yocto \
+        yocktodocterquickstart \
         /bin/bash
 else
     docker container run \
         -it \
         --rm \
         -v "${PWD}":/opt/yocto \
-        --name hubshuffle-yocto \
+        --name yoctodockerquickstart \
         ${arg_net_forward} \
         ${arg_x11_forward} \
         ${arg_privileged} \
         --volume "${PWD}/home":/home/yocto \
-        hubshuffle/yocto:1.2 \
+        yoctodockerquickstart:latest \
         sudo bash -c "groupadd -g 7777 yocto && useradd --password ${empty_password_hash} --shell /bin/bash -u ${UID} -g 7777 \
         yocto && usermod -aG sudo yocto && usermod -aG users yocto && cd /opt/yocto && su yocto"
 fi
